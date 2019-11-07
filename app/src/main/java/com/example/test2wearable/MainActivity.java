0edffa270b0bfb209ac78ed72603e445eaa2ff1f
Pintu jat <pintu.chaudhary9@gmail.com>
2019-11-07 11:07:18 -0500

    Timer Added

--- app/src/main/java/com/example/test2wearable/MainActivity.java
+++ app/src/main/java/com/example/test2wearable/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+
 import io.particle.android.sdk.cloud.ParticleCloud;
 import io.particle.android.sdk.cloud.ParticleCloudSDK;
 import io.particle.android.sdk.cloud.ParticleDevice;
@@ -12,7 +15,11 @@ import io.particle.android.sdk.cloud.ParticleEvent;
 import io.particle.android.sdk.cloud.ParticleEventHandler;
 import io.particle.android.sdk.cloud.exceptions.ParticleCloudException;
 import io.particle.android.sdk.utils.Async;
+
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,7 +30,25 @@ public class MainActivity extends AppCompatActivity {
     private final String DEVICE_ID = "2d0033000447363333343435";
     private long subscriptionId;
     private ParticleDevice mDevice;
-    TextView answer;
+    TextView startTimer;
+    long startTime = 0;
+    Timer timer = new Timer();
+
+    Handler timerHandler = new Handler();
+    Runnable timerRunnable = new Runnable() {
+
+        @Override
+        public void run() {
+            long millis = System.currentTimeMillis() - startTime;
+            int seconds = (int) (millis / 1000);
+            int minutes = seconds / 60;
+            seconds = seconds % 60;
+
+            startTimer.setText(String.format("%d",seconds));
+
+            timerHandler.postDelayed(this, 500);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +56,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ParticleCloudSDK.init(this.getApplicationContext());
         getDeviceFromCloud();
+
+        startTimer = (TextView) findViewById(R.id.timerText);
+
+        Button b = (Button) findViewById(R.id.startButton);
+        b.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Button b = (Button) v;
+                if (b.getText().equals("stop")) {
+                    timerHandler.removeCallbacks(timerRunnable);
+                    b.setText("start");
+                } else {
+                    startTime = System.currentTimeMillis();
+                    timerHandler.postDelayed(timerRunnable, 0);
+                    b.setText("stop");
+                }
+            }
+        });
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        timerHandler.removeCallbacks(timerRunnable);
+        Button b = (Button)findViewById(R.id.startButton);
+        b.setText("start");
     }
 
     public void getDeviceFromCloud() {
